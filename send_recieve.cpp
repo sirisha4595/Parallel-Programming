cfd5fe6eaaf923603f2c74817ca2bf0d2893ce43
Sagar Jeevan <tg856233@c455-134.stampede2.tacc.utexas.edu>
2019-05-08 22:21:26 -0500

    a few modifications. working now

--- send_recieve.cpp
+++ send_recieve.cpp
@@ -5,17 +5,19 @@ using namespace std;
 int main(int argc, char *argv[ ])
 {
 	int p, myrank, v = 121;
+	int s_tag = 10;
 	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	
+
 	if (myrank == 0) {
-		MPI_Send(&v, 1, MPI_INT, 1, MPI_ANY_TAG, MPI_COMM_WORLD);
+		MPI_Send(&v, 1, MPI_INT, 1, s_tag, MPI_COMM_WORLD);
 		cout << "Process " << myrank << " sent " << v << endl;
-	} else if (myrank == 1) {
-		MPI_Recv(&v, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+	} 
+	if (myrank == 1) {
+		MPI_Recv(&v, 1, MPI_INT, 0, s_tag, MPI_COMM_WORLD, &status);
 		cout << "Process " << myrank << " receieved " << v << endl;
 	}
 
